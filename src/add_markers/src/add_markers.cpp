c4899d84ae83e0525f285bc68b3d9050ef853e76
Erdi Akyuz <erdiakyuz@gmail.com>
2019-11-13 12:15:23 +0100

    Home Service Robot added

--- src/add_markers/src/add_markers.cpp
+++ src/add_markers/src/add_markers.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
 #include <nav_msgs/Odometry.h>
+#include <math.h>
 
 
 
@@ -9,13 +10,22 @@ double startY= 1.0;
 double endX= 2.0;
 double endY= 1.0;
 
-
+bool isItemPicked=false;
+bool isItemDropped=false;
 
 void odomCallback(const nav_msgs::Odometry::ConstPtr& msg) {
-  double Robotx = msg->pose.pose.position.x;
-  double Roboty = msg->pose.pose.position.y;
+  double RobotX = msg->pose.pose.position.x;
+  double RobotY = msg->pose.pose.position.y;
 
-  ROS_INFO("Message received!");
+  double distance2Start=sqrt(pow(RobotX-startX, 2) + pow(RobotY-startY, 2));
+  double distance2End=sqrt(pow(RobotX-endX, 2) + pow(RobotY-endY, 2));
+
+  if (distance2Start < 0.4) {
+      isItemPicked = true;
+    }
+else if (distance2End < 0.8) {
+      isItemDropped = true;
+    }
 }
 
 
@@ -32,45 +42,46 @@ int main( int argc, char** argv )
   // Set our initial shape type to be a cube
   uint32_t shape = visualization_msgs::Marker::CUBE;
 
+  visualization_msgs::Marker marker;
+  // Set the frame ID and timestamp.  See the TF tutorials for information on these.
+  marker.header.frame_id = "map";
+  marker.header.stamp = ros::Time::now();
+
+  // Set the namespace and id for this marker.  This serves to create a unique ID
+  // Any marker sent with the same namespace and id will overwrite the old one
+  marker.ns = "cube";
+  marker.id = 0;
+
+  // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
+  marker.type = shape;
+
+
+
+  // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
+  marker.pose.position.x = startX;
+  marker.pose.position.y = startY;
+  marker.pose.position.z = 0;
+  marker.pose.orientation.x = 0.0;
+  marker.pose.orientation.y = 0.0;
+  marker.pose.orientation.z = 0.0;
+  marker.pose.orientation.w = 1.0;
+
+  // Set the scale of the marker -- 1x1x1 here means 1m on a side
+  marker.scale.x = 0.2;
+  marker.scale.y = 0.2;
+  marker.scale.z = 0.2;
+
+  // Set the color -- be sure to set alpha to something non-zero!
+  marker.color.r = 0.0f;
+  marker.color.g = 1.0f;
+  marker.color.b = 0.0f;
+  marker.color.a = 1.0;
+
+  marker.lifetime = ros::Duration();
+
+
   while (ros::ok())
   {
-    visualization_msgs::Marker marker;
-    // Set the frame ID and timestamp.  See the TF tutorials for information on these.
-    marker.header.frame_id = "map";
-    marker.header.stamp = ros::Time::now();
-
-    // Set the namespace and id for this marker.  This serves to create a unique ID
-    // Any marker sent with the same namespace and id will overwrite the old one
-    marker.ns = "cube";
-    marker.id = 0;
-
-    // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
-    marker.type = shape;
-
-    // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
-    marker.action = visualization_msgs::Marker::ADD;
-
-    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-    marker.pose.position.x = startX;
-    marker.pose.position.y = startY;
-    marker.pose.position.z = 0;
-    marker.pose.orientation.x = 0.0;
-    marker.pose.orientation.y = 0.0;
-    marker.pose.orientation.z = 0.0;
-    marker.pose.orientation.w = 1.0;
-
-    // Set the scale of the marker -- 1x1x1 here means 1m on a side
-    marker.scale.x = 1.0;
-    marker.scale.y = 1.0;
-    marker.scale.z = 1.0;
-
-    // Set the color -- be sure to set alpha to something non-zero!
-    marker.color.r = 0.0f;
-    marker.color.g = 1.0f;
-    marker.color.b = 0.0f;
-    marker.color.a = 1.0;
-
-    marker.lifetime = ros::Duration();
 
     // Publish the marker
     while (marker_pub.getNumSubscribers() < 1)
@@ -82,51 +93,30 @@ int main( int argc, char** argv )
       ROS_WARN_ONCE("Please create a subscriber to the marker");
       sleep(1);
     }
-    marker_pub.publish(marker);
+
+    if (isItemPicked==false){
+      // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
+      marker.action = visualization_msgs::Marker::ADD;
+      marker_pub.publish(marker);
+}
+  else if (isItemDropped==false)
+{
+  marker.action = visualization_msgs::Marker::DELETE;
+  marker_pub.publish(marker);
+}
+else if (isItemDropped)
+{
+  // Go to Sleep for 1 seconds
+    ros::Duration(2.0).sleep();
+  marker.pose.position.x = endX;
+  marker.pose.position.y = endY;
+  marker.action = visualization_msgs::Marker::ADD;
+  marker_pub.publish(marker);
+}
 
 
-ROS_INFO("Passed first loop!");
-    visualization_msgs::Marker::CUBE;
 
- // Go to Sleep for 5 seconds
-    ros::Duration(5.0).sleep();
-
-
-
-// Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
-    marker.action = visualization_msgs::Marker::DELETE;
-    marker_pub.publish(marker);
-ROS_INFO("Marker Deleted!");
-// Go to Sleep for 5 seconds
-    ros::Duration(5.0).sleep();
-
-    // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
-    marker.action = visualization_msgs::Marker::ADD;
-
-
-    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-    marker.pose.position.x = endX;
-    marker.pose.position.y = endY;
-
-
-    // Publish the marker
-    while (marker_pub.getNumSubscribers() < 1)
-    {
-      if (!ros::ok())
-      {
-        return 0;
-      }
-      ROS_WARN_ONCE("Please create a subscriber to the marker");
-      sleep(1);
-    }
-    ROS_INFO("Passed the 2nd Loop!");
-    visualization_msgs::Marker::CUBE;
-
-    marker_pub.publish(marker);
-
-
-    // Go to Sleep for 5 seconds
-       ros::Duration(5.0).sleep();
+ros::spinOnce();
 
 
 
